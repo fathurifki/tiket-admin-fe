c02734ec0f0c86f1717dfa96036223717a279f40
fathurifki <rifkifathu@gmail.com>
2023-11-27 11:04:12 +0700

    fixing quill

--- components/atoms/WYSIWYG/index.jsx
+++ components/atoms/WYSIWYG/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 import 'react-quill/dist/quill.snow.css';
 
-const QuillNoSSRWrapper = dynamic(import("react-quill"), {
+const QuillNoSSRWrapper = dynamic(() => import("react-quill"), {
   ssr: false,
   loading: () => <p>Loading ...</p>,
 });
