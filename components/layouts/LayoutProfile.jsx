591e5246408376f60fcdda5dc3b844d62c398bf1
fathurifki <rifkifathu@gmail.com>
2023-11-27 13:59:08 +0700

    fixing bug

--- components/layouts/LayoutProfile.jsx
+++ components/layouts/LayoutProfile.jsx
@@ -19,6 +19,7 @@ import { LogOutIcon } from "lucide-react";
 import LoginPage from "../template/LoginPage";
 import { deleteCookie, getCookie } from "cookies-next";
 import { useRouter } from "next/router";
+import ImageAssets from "../assets";
 
 function Sidebar({ menuItems }) {
   const router = useRouter();
@@ -33,13 +34,6 @@ function Sidebar({ menuItems }) {
       <div>
         <div className="mb-4 p-2">
           <div className="flex gap-2 items-center">
-            <Image
-              src="https://s3-alpha-sig.figma.com/img/d4f5/da8d/0ce1f62fdcad1fa0ca39a16d21e57e7b?Expires=1699833600&Signature=WyRcuAAZDixG3k57Ao69DDBj1jOmPNzEEsfFPdIUeRFAcf5Z1r4fWk2kgwcLpT~0OWAPEAqpbin~1zDCfQCs-tvCc355x5U3jDOwrc~YL8JFd99u6Rc0D1OaM88j8qAFeKuP6q2MjsnQBJ9aZfU-OJNm-zpD1sooufsw~askQqHW9nU12tgWqaLiWdD6krMAIA5VasCnKyOGCQYS-yjsKMPcXHkYhrW7SjEwwEbeF-N0F8w6Wbu19m1bKIBz92PQszKKtnmDCymWCCxabXds98g7bar2qxJCsFqH8F4Ryje-9VJSIiRlwQ79uTGvEnyImOfnCoct05ywOyOVEA7Eiw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-              alt="User"
-              width={10}
-              height={10}
-              className="w-10 rounded-full bg-black p-2 lg:h-10"
-            />
             <h1 className="font-bold mb-2 lg:block hidden">
               Mantra Botol Kreatif
             </h1>
@@ -56,7 +50,7 @@ function Sidebar({ menuItems }) {
           ))}
         </div>
       </div>
-      <div onClick={logoutButton} className="absolute bottom-0 flex pb-6 gap-2 lg:items-start md:items-center">
+      <div onClick={logoutButton} className="absolute bottom-0 flex pb-6 gap-2 lg:items-start md:items-center cursor-pointer">
         <LogOutIcon />
         <span className="hidden lg:block">Logout</span>
       </div>
--- components/template/LoginPage/index.jsx
+++ components/template/LoginPage/index.jsx
@@ -20,6 +20,7 @@ import fetchingData from "@/lib/api";
 import { setCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import ImageAssets from "@/components/assets";
 
 const FormSchema = z.object({
   username: z.string().min(1, {
@@ -71,13 +72,6 @@ const LoginPage = ({ onLogin }) => {
   return (
     <div className="grid grid-cols w-full h-screen lg:grid lg:grid-cols-2 ">
       <div className="hidden lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-full lg:w-full lg:border-r bg-black">
-        <Image
-          width={120}
-          height={120}
-          src="https://s3-alpha-sig.figma.com/img/d4f5/da8d/0ce1f62fdcad1fa0ca39a16d21e57e7b?Expires=1699833600&Signature=WyRcuAAZDixG3k57Ao69DDBj1jOmPNzEEsfFPdIUeRFAcf5Z1r4fWk2kgwcLpT~0OWAPEAqpbin~1zDCfQCs-tvCc355x5U3jDOwrc~YL8JFd99u6Rc0D1OaM88j8qAFeKuP6q2MjsnQBJ9aZfU-OJNm-zpD1sooufsw~askQqHW9nU12tgWqaLiWdD6krMAIA5VasCnKyOGCQYS-yjsKMPcXHkYhrW7SjEwwEbeF-N0F8w6Wbu19m1bKIBz92PQszKKtnmDCymWCCxabXds98g7bar2qxJCsFqH8F4Ryje-9VJSIiRlwQ79uTGvEnyImOfnCoct05ywOyOVEA7Eiw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-          alt="User"
-          className="p-2"
-        />
         <p className="font-bold mb-2 lg:block hidden text-white text-3xl">
           Mantra Botol Kreatif
         </p>
